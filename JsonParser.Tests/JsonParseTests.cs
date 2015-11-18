b5dad50c91029e498be8a6247f39c16949f165d7
Brian Friesen <brianfriesen@quickenloans.com>
2015-11-18 18:35:44 -0500

    true

--- JsonParser.Tests/JsonParseTests.cs
+++ JsonParser.Tests/JsonParseTests.cs
@@ -1,7 +1,17 @@
-﻿namespace JsonParser.Tests
+﻿using NUnit.Framework;
+
+namespace JsonParser.Tests
 {
     public class JsonParseTests
     {
-         
+        [Test, Ignore]
+        public void TrueReturnsTrue()
+        {
+            var json = "true";
+
+            var result = Json.Parse(json);
+
+            Assert.That(result, Is.True);
+        }
     }
 }
