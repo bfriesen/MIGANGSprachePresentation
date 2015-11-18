74ee6b661d358f1829cf9540374b2f2cc9b279c9
Brian Friesen <brianfriesen@quickenloans.com>
2015-11-18 18:42:40 -0500

    false

--- JsonParser.Tests/JsonParseTests.cs
+++ JsonParser.Tests/JsonParseTests.cs
@@ -14,7 +14,7 @@ namespace JsonParser.Tests
             Assert.That(result, Is.True);
         }
 
-        [Test, Ignore]
+        [Test]
         public void FalseReturnsFalse()
         {
             var json = "false";
